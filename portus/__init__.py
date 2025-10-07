aa057bd1ac103862f50ac2006cba9b5708f0ab06
Marko Hostnik <marko.hostnik@jetbrains.com>
2025-10-07 19:18:33 +0200

    Apply nest_asyncio workaround
    # Conflicts:
    #	portus/__init__.py
    #	pyproject.toml
    #	uv.lock

--- portus/__init__.py
+++ portus/__init__.py
@@ -1,6 +1,12 @@
+import nest_asyncio  # type: ignore[import-untyped]
+
 from .api import open_session
 from .configs.llm import LLMConfig
 from .core import ExecutionResult, Executor, Opa, Pipe, Session, VisualisationResult, Visualizer
+
+# Workaround to allow asyncio.run() inside Jupyter notebooks.
+nest_asyncio.apply()
+
 
 __all__ = [
     "open_session",

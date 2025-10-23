1e6816fe8dc4db119f8e3329573c2ff45cbc14da
Marko Hostnik <marko.hostnik@jetbrains.com>
2025-10-23 14:19:20 +0200

    Revert "Apply nest_asyncio workaround"
    This reverts commit 8d4ea327

--- portus/__init__.py
+++ portus/__init__.py
@@ -1,11 +1,6 @@
-import nest_asyncio  # type: ignore[import-untyped]
-
 from portus.api import open_session
 from portus.configs.llm import LLMConfig
 from portus.core import ExecutionResult, Executor, Opa, Pipe, Session, VisualisationResult, Visualizer
-
-# Workaround to allow asyncio.run() inside Jupyter notebooks.
-nest_asyncio.apply()
 
 __all__ = [
     "ExecutionResult",
